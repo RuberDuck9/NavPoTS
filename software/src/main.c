6aae7dfcb723f45262c540d6ffff5c1229b834b0
RuberDuck9 <github.header392@passmail.net>
2026-07-08 22:58:48 -0400

    add io library

--- software/src/main.c
+++ software/src/main.c
@@ -1,5 +1,14 @@
-int main(){
+#include "io.h"
 
+int main(void){
+
+	set_pin(RCC_GPIOE, GPIOE, GPIO14);
+	
+	for (int i = 0; i < 999999999; i++){
+		__asm__("nop");
+	}
+
+	clear_pin(RCC_GPIOE, GPIOE, GPIO15);
 
 	return 0;
 }
